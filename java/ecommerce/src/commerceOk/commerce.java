3a81c05fd2670b7d28eed7a8622466b149340f3b
Gustavo Miquéias <gumiqueias@hotmail.com>
2020-11-16 08:01:24 -0300

    Generation

--- java/ecommerce/src/commerceOk/commerce.java
+++ java/ecommerce/src/commerceOk/commerce.java
@@ -1,6 +1,8 @@
+package commerceOk;
+
 import java.util.Scanner;
 
-public class e_commerce {
+public class commerce {
 
 	public static void main(String[] args) {
 		
@@ -16,6 +18,7 @@ public class e_commerce {
 		int codigo[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int vezes = 0, parc = 0;
 		double valorCompras[] = new double[minimo];
+		double valorSoma[] = new double[minimo];
 		double valorTotal = 0.0;
 		double imp = 0.0;
 		double valorAtualizado = 0.0;
@@ -63,10 +66,8 @@ public class e_commerce {
 					
 					if ((estoque[0] - quantidade[0]) < 0) 
 					{
-						System.out.println("\nQUANTIDADE FORA DE ESTOQUE !! ");
-						System.out.print("Quantas unidades do produto "+produtos[0]+" :");
-						quantidade[x] = ler.nextInt();
-
+						System.out.println("\nQUANTIDADE FORA DE ESTOQUE !!\n");
+						compras [0] = null;
 					}
 					else 
 					{
@@ -74,9 +75,12 @@ public class e_commerce {
 						compras[0] = produtos[0];
 						totalQuantidade[0] = totalQuantidade[0] + quantidade[0];
 						valorCompras[0] = (valor[0] * quantidade[0]);
+						valorCompras[0] =valorCompras[0] + valorCompras[0];
+						valorSoma[0] = valorSoma[0] + valorCompras[0];	
+						System.out.println("O produto "+"'"+produtos[0]+"'"+ " foi adicionado ao carrinho.\n");
 					}
 					
-					System.out.println("O produto "+"'"+produtos[0]+"'"+ " foi adicionado ao carrinho.\n");
+					
 					System.out.println("Deseja adicionar outros produtos ao carrinho ?(S/N)");
 					confirmacao = ler.next().charAt(0);
 					
@@ -98,10 +102,8 @@ public class e_commerce {
 					
 					if ((estoque[1] - quantidade[1]) < 0) 
 					{
-						System.out.println("\nQUANTIDADE FORA DE ESTOQUE !! ");
-						System.out.print("Quantas unidades do produto "+produtos[1]+" :");
-						quantidade[x] = ler.nextInt();
-
+						System.out.println("\nQUANTIDADE FORA DE ESTOQUE !! \n");
+						compras [1] = null;
 					}
 					else 
 					{
@@ -109,6 +111,7 @@ public class e_commerce {
 					compras[1] = produtos[1];
 					totalQuantidade[1] = totalQuantidade[1] + quantidade[1];;
 					valorCompras[1] = (valor[1] * quantidade[1]);
+					valorSoma[1] = valorSoma[1] + valorCompras[1];
 					}
 					
 					System.out.println("O produto "+"'"+produtos[1]+"'"+ " foi adicionado ao carrinho.\n");
@@ -143,6 +146,7 @@ public class e_commerce {
 						compras[2] = produtos[2];
 						totalQuantidade[2] = totalQuantidade[2] + quantidade[2];
 						valorCompras[2] = (valor[2] * quantidade[2]);
+						valorSoma[2] = valorSoma[2] + valorCompras[2];
 					}
 					
 					System.out.println("O produto "+"'"+produtos[2]+"'"+ " foi adicionado ao carrinho.\n");
@@ -177,6 +181,7 @@ public class e_commerce {
 						compras[3] = produtos[3];
 						totalQuantidade[3] = totalQuantidade[3] + quantidade[3];
 						valorCompras[3] = (valor[3] * quantidade[3]);
+						valorSoma[3] = valorSoma[3] + valorCompras[3];
 					}
 					
 					System.out.println("O produto "+"'"+produtos[3]+"'"+ " foi adicionado ao carrinho.\n");
@@ -211,6 +216,7 @@ public class e_commerce {
 						compras[4] = produtos[4];
 						totalQuantidade[4] = totalQuantidade[4] + quantidade[4];
 						valorCompras[4] = (valor[4] * quantidade[4]);
+						valorSoma[4] = valorSoma[4] + valorCompras[4];
 					}
 					
 					System.out.println("O produto "+"'"+produtos[4]+"'"+ " foi adicionado ao carrinho.\n");
@@ -245,6 +251,7 @@ public class e_commerce {
 						compras[5] = produtos[5];
 						totalQuantidade[5] = totalQuantidade[5] + quantidade[5];
 						valorCompras[5] = (valor[5] * quantidade[5]);
+						valorSoma[5] = valorSoma[5] + valorCompras[5];
 					}
 					
 					System.out.println("O produto "+"'"+produtos[5]+"'"+ " foi adicionado ao carrinho.\n");
@@ -279,6 +286,7 @@ public class e_commerce {
 						compras[6] = produtos[6];
 						totalQuantidade[6] = totalQuantidade[6] + quantidade[6];
 						valorCompras[6] = (valor[6] * quantidade[6]);
+						valorSoma[6] = valorSoma[6] + valorCompras[6];
 					}
 					System.out.println("O produto "+"'"+produtos[6]+"'"+ " foi adicionado ao carrinho.\n");
 					System.out.println("Deseja adicionar outros produtos ao carrinho ?(S/N)");
@@ -311,6 +319,7 @@ public class e_commerce {
 						compras[7] = produtos[7];
 						totalQuantidade[7] = totalQuantidade[7] + quantidade[7];
 						valorCompras[7] = (valor[7] * quantidade[7]);
+						valorSoma[7] = valorSoma[7] + valorCompras[7];
 					}
 					System.out.println("O produto "+"'"+produtos[7]+"'"+ " foi adicionado ao carrinho.\n");
 					System.out.println("Deseja adicionar outros produtos ao carrinho ?(S/N)");
@@ -342,6 +351,7 @@ public class e_commerce {
 						compras[8] = produtos[8];
 						totalQuantidade[8] = totalQuantidade[8] + quantidade[8];
 						valorCompras[8] = (valor[8] * quantidade[8]);
+						valorSoma[8] = valorSoma[8] + valorCompras[8];
 					}
 					System.out.println("O produto "+"'"+produtos[8]+"'"+ " foi adicionado ao carrinho.\n");
 					System.out.println("Deseja adicionar outros produtos ao carrinho ?(S/N)");
@@ -375,6 +385,7 @@ public class e_commerce {
 						compras[9] = produtos[9];
 						totalQuantidade[9] = totalQuantidade[9] + quantidade[9];
 						valorCompras[9] = (valor[9] * quantidade[9]);
+						valorSoma[9] = valorSoma[9] + valorCompras[9];
 					}
 					System.out.println("O produto "+"'"+produtos[9]+"'"+ " foi adicionado ao carrinho.\n");
 					System.out.println("Deseja adicionar outros produtos ao carrinho ?(S/N)");
@@ -390,9 +401,6 @@ public class e_commerce {
 					}
 				}
 				
-				if(estoque[x] == 0) {
-					System.out.println("\nProduto fora de estoque.");
-				}
 			}
 			
 				System.out.println("\n--------------CARRINHO DE COMPRAS---------------:\n");
@@ -400,12 +408,12 @@ public class e_commerce {
 				
 				for (x = 0; x < minimo; x++ )
 				{
-					valorTotal = valorTotal + valorCompras[x];
+					valorTotal = valorTotal + valorSoma[x];
 					
 					if (compras[x] != null) 
 					{
 										
-					System.out.println((x + 1)+" - "+compras[x]+"\t"+"R$ "+ valorCompras[x]+"\t"+totalQuantidade[x]+" - und.\n");
+					System.out.println((x + 1)+" - "+compras[x]+"\t"+"R$ "+ valorSoma[x]+"\t\t"+totalQuantidade[x]+" - und.\n");
 					
 					}				
 				}
@@ -461,60 +469,59 @@ public class e_commerce {
 				notaFiscal = ler.next().toUpperCase().charAt(0);
 		        
 		       
-		        if (notaFiscal == 'S')
-				{
-					System.out.println("\n\n\n\n\n------------------------------NOTA FISCAL----------------------------\n");
-					System.out.println("\n--------------------------LOJAS LUIS VINTÃO--------------------------\n");
-					
-					System.out.println("\t\tPRODUTOS:\t"+"VALOR:\t\tQUANTIDADE:\n");
-					
-					for(x = 0; x < minimo; x++ )
+				 if (notaFiscal == 'S')
 					{
-						if ((compras[x] != null) || (valorCompras[x] > 0.0)) 
-						{							
-							
-							System.out.println("\t\t"+(x + 1)+" - "+compras[x]+"\t"+"R$ "+ valorCompras[x]+"\t\t"+totalQuantidade[x]+" - und.\n");
+						System.out.println("\n\n\n\n\n------------------------------NOTA FISCAL----------------------------\n");
+						System.out.println("\n--------------------------LOJAS LUIS VINTÃO--------------------------\n");
 						
+						System.out.println("\t\tPRODUTOS:\t"+"VALOR:\t\tQUANTIDADE:\n");
+						
+						for(x = 0; x < minimo; x++ )
+						{
+							if ((compras[x] != null) || (valorSoma[x] > 0.0)) 
+							{							
+								
+								System.out.println("\t\t"+(x + 1)+" - "+compras[x]+"\t"+"R$ "+ valorSoma[x]+"\t\t"+totalQuantidade[x]+" - und.\n");
+							
+							}
+																	
 						}
 						
-									
-					}
+						System.out.printf("---------------------IMPOSTOS A SEREM PAGOS R$ %.2f------------------",imp,"\n\n");			
+						
+						if (vezes == 1)
+						{ 
+							System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
+							valorTotal = (valorTotal / 2);
+							System.out.printf("\n----------------------PARCELADO EM 2X DE R$ %.2f -------------------" ,valorTotal,"\n\n");
+						}
+						else if (vezes == 2)
+						{ 
+							System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
+							valorTotal = (valorTotal / 3);
+							System.out.printf("\n----------------------PARCELADO EM 3X DE R$ %.2f -------------------" ,valorTotal,"\n\n");
+						}
+						else
+						{
+							System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
+						}
+						System.out.print("\n\n------------------------OBRIGADO E VOLTE SEMPRE----------------------\n");
+						
+					} 
+					System.out.print("\n\nDeseja continuar com novas compras ? (S/N)\n" );
+					novoUser = ler.next().charAt(0);
 					
-					System.out.printf("---------------------IMPOSTOS A SEREM PAGOS R$ %.2f------------------",imp,"\n\n");			
-					
-					if (vezes == 1)
-					{ 
-						System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
-						valorTotal = (valorTotal / 2);
-						System.out.printf("\n----------------------PARCELADO EM 2X DE R$ %.2f -------------------" ,valorTotal,"\n\n");
-					}
-					else if (vezes == 2)
-					{ 
-						System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
-						valorTotal = (valorTotal / 3);
-						System.out.printf("\n----------------------PARCELADO EM 3X DE R$ %.2f -------------------" ,valorTotal,"\n\n");
-					}
-					else
+					if (novoUser == 'S' || novoUser== 's') {
+						user = true;
+						valorTotal = 0.0;
+					} 
+					else 
 					{
-						System.out.printf("\n\n----------------------TOTAL A SER PAGO = R$ %.2f -------------------\n",valorTotal);
+						user = false;	
+						System.out.print("\n\n------------------------OBRIGADO E VOLTE SEMPRE-----------------------\n");
 					}
-					System.out.print("\n\n------------------------OBRIGADO E VOLTE SEMPRE----------------------\n");
-					
-				} 
-				System.out.print("\n\nDeseja continuar com novas compras ? (S/N)\n" );
-				novoUser = ler.next().charAt(0);
-				
-				if (novoUser == 'S' || novoUser== 's') {
-					user = true;
-					valorTotal = 0.0;
-				} 
-				else 
-				{
-					user = false;	
-					System.out.print("\n\n------------------------OBRIGADO E VOLTE SEMPRE----------------------\n");
-				}
+			}
+			
 		}
-		
-	}
 
-}
+	}
